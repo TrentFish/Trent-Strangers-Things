2e970e39babdcf880004b93c149085c1d789b7d8
fishyboi@MSI <fishyboi@MSI>
2023-09-14 14:15:24 -0400

    about us

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,7 +1,8 @@
 const AboutUs = ()=> {
   return (
     <div>
-      TODO: Add some information about Us!
+      <h2>We allow our users to sell strange things.</h2>
+      <h4>If you're interested, register now!</h4>
     </div>
   );
 };
